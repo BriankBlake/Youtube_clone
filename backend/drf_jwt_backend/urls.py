d663ebaca5d0afe3ab5e5351edf3f79547cce834
brian blake <104259527+babyslikk@users.noreply.github.com>
2022-08-30 15:50:52 -0500

    Updated the urls.py file

--- backend/drf_jwt_backend/urls.py
+++ backend/drf_jwt_backend/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from xml.etree.ElementInclude import include
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('api/auth/', include('authentication.urls')),
+    path('api/cars/', include('cars.urls')),
+    path('api/comments/', include('comments.urls')),
 ]
