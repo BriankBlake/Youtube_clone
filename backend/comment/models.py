e3d3b57e30c33e3ad2345cfcf0f76f86867de543
brian blake <104259527+babyslikk@users.noreply.github.com>
2022-09-01 22:01:39 -0500

    Created models in the models.py filed

--- backend/comment/models.py
+++ backend/comment/models.py
@@ -4,9 +4,9 @@ from django.db import models
 from backend.authentication.models import User
 
 # Create your models here.
-class comment ():
+class comment (models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    video_id = models.CharField()
-    text = models.CharField()
+    video_id = models.CharField(max_length=255)
+    text = models.CharField(max_length=255)
     likes = models.IntegerField()
     dislikes = models.IntegerField()
