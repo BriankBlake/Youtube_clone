8444cf62c1bce79e67cec465b3f36dc59d7d92b1
brian blake <104259527+babyslikk@users.noreply.github.com>
2022-09-01 22:30:07 -0500

    Updated the serializers.py file

--- backend/comment/models.py
+++ backend/comment/models.py
@@ -1,10 +1,10 @@
-from ast import Delete
 from django.db import models
+from authentication.models import User
 
 from backend.authentication.models import User
 
 # Create your models here.
-class comment (models.Model):
+class Comment (models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     video_id = models.CharField(max_length=255)
     text = models.CharField(max_length=255)
