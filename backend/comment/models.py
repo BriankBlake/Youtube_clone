8e4518a3a8118831f718cc14c96cf0cabd3147e5
brian blake <104259527+babyslikk@users.noreply.github.com>
2022-09-01 21:56:34 -0500

    Created code for the models.py in the comments folder

--- backend/comment/models.py
+++ backend/comment/models.py
@@ -1,5 +1,12 @@
+from ast import Delete
 from django.db import models
+
+from backend.authentication.models import User
 
 # Create your models here.
 class comment ():
-    
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    video_id = models.CharField()
+    text = models.CharField()
+    likes = models.IntegerField()
+    dislikes = models.IntegerField()
