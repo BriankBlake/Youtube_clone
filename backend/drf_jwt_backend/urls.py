1025f20952277339cf55125eab080e0b7cca4966
brian blake <104259527+babyslikk@users.noreply.github.com>
2022-09-01 21:29:15 -0500

    Created a comment fold

--- backend/drf_jwt_backend/urls.py
+++ backend/drf_jwt_backend/urls.py
@@ -21,8 +21,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-   path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('api/auth/', include('authentication.urls')),
     path('api/cars/', include('cars.urls')),
-    # path('api/comments/', include('comments.urls')),
+    path('api/comments/', include('comments.urls')),
 ]
