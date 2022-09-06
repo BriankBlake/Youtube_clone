5d402e24c35e58c9ea47d7502844e2ebc2b30545
brian blake <104259527+babyslikk@users.noreply.github.com>
2022-09-06 13:16:00 -0500

    Updated + fix errors in the setting file + Created postman collection

--- backend/comments/urls.py
+++ backend/comments/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from comments import views
 
 urlpatterns = [
-    path('<str:id>/', views.get_all_comment),
     path('', views.new_comment),
+    path('<str:id>/', views.get_all_comment),
     path('like_status/<str:id>/', views.likes)
 ]
