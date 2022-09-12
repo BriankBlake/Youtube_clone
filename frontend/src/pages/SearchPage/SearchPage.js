a29798e55ca0edfc07a3a29e4ad0846f983d6a25
brian blake <104259527+babyslikk@users.noreply.github.com>
2022-09-12 16:25:20 -0500

    Created SearchPage.js

---
+++ frontend/src/pages/SearchPage/SearchPage.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from 'react';
+import {KEY} from "../../localKey";
+import axios from "axios";
+import {Link, useParams} from "react-router-dom";
+
+const SearchPage = () => {
+    const [videos, setVideos] = useState([]);
+    const {search} = useParams();
+
+   
+    const fetchVideos = async (search) => {
+        try {
+            let response = await axios.get(`https://www.googleapis.com/youtube/v3/search?q=${search}&key=${KEY}`);
+            console.log(response.data.items)
+            setVideos(response.data.items)
+        } catch (error) {
+            
+        }
+    };
+    useEffect(() => {
+        fetchVideos();
+    }, []);
+
+    return (
+        <div className='videoscontainer'>
+            {videos &&
+            videos.map((video) => {
+                return (
+                    <div className='vid' key={video.snippet.title}>
+                        <p>{video.snippet.title}</p>
+                        <Link to={`/video/${video.id.videoId}`}>
+                            <img src={video.snippet.thumbnails.medium.url} />
+                        </Link>
+
+                    </div>
+                );
+            })}
+        </div>
+    );
+}
+;
+export default SearchPage;
