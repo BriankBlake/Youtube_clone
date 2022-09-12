61011d165c2ad6e9848714defd8767b8225ea9f8
brian blake <104259527+babyslikk@users.noreply.github.com>
2022-09-12 11:18:57 -0500

    Created SearchBar.jsx file

---
+++ frontend/src/components/SearchBar.jsx
@@ -0,0 +1,23 @@
+import React, {useState} from 'react';
+import { useNavigate } from 'react-router-dom';
+import SearchPage from '../../pages/SearchPage';
+
+const SearchBar = (props) => {
+    const [search, updateSearch] = useState();
+
+    const navigate = useNavigate();
+
+    function handleSubmit (event) {
+        event.preventDefault();
+        const encoded = encodeURIComponent (search);
+        navigate (`/search/${search}`);
+    }
+    return (
+        <form onSubmit={handleSubmit}>
+            <input type="search" value={search} onChange={ (event) => updateSearch (event.target.value) } />
+            <button>Search</button>
+            </form>
+    );
+};
+
+export default SearchBar;
