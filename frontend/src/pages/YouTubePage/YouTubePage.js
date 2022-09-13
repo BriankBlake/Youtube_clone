e379ae2386b6083c198ed5d2dce4c4cee457ec5a
brian blake <104259527+babyslikk@users.noreply.github.com>
2022-09-12 22:47:03 -0500

    finish working on the youtubepage

---
+++ frontend/src/pages/YouTubePage/YouTubePage.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from 'react';
+import { DATA } from '../../localData';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import {KEY} from '../../localKey';
+import "./YouTubePage.css";
+
+ const YouTubePage = () => {
+    const [videos, setVideos] = useState([]);
+
+    useEffect(() => {
+        fetchVideos()
+    }, []);
+
+    const fetchVideos = async () => {
+        try {
+            let response = await axios.get(`https://www.googleapis.com/youtube/v3/search?q=stranger%20things&key=${KEY}&part=snippet&maxResults=10`);
+            console.log(response.data.items)
+            setVideos(response.data.items)
+        } catch (error) {
+            console.log(error.message);   
+        }
+    };
+    return (
+        <div className='videoContainer'>
+            {videos &&
+            videos.map((video) => {
+                return(
+                    <div className='video' key={video.snippet.title}>
+                        <p>{video.snippet.title}</p>
+                        <Link to={`/video/${video.id.videoId}`}>
+                            <img src={video.snippet.thumbnails.medium.url} />
+                        </Link>
+                    </div>
+                );
+
+            })}
+        </div>
+    );
+ };
+
+ export default YouTubePage;
