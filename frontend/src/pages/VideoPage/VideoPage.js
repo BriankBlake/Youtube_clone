109d8ff78868316b1efdbb2acc8167f0cce7357b
brian blake <104259527+babyslikk@users.noreply.github.com>
2022-09-12 21:37:56 -0500

    Made changing to the VideoPage.js file

---
+++ frontend/src/pages/VideoPage/VideoPage.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { KEY } from '../../localkey';
+import './VideoPage.css';
+
+const VideoPage = (props) => {
+    const {videoid} = useParams();
+    console.log(videoid)
+    const [relatedVideos, setRelatedVideos] = useState([]);
+
+    async function getRelatedVideos(id) {
+        try {
+            let response = await axios.get(
+                `https://www.googleapis.com/youtube/v3/search?relatedToVideoId=${id}&type=video&key=${KEY}&part=snippet&maxResults=8`);
+                setRelatedVideos(response.data.items);
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
+}
+useEffect(() => {
+    getRelatedVideos(videoid);
+ },[]);
+
+return (
+<div className='video-page'>
+    <div>
+    <iframe
+        id="ytplayer"
+        type="text/html"
+        title="myVideo"
+        width="640"
+        height="360"
+        src={`https://www.youtube.com/embed/${videoid}?autoplay=1`}
+      ></iframe>
+    </div>
+    <div className='relatedvideos'>
+        {relatedVideos &&
+        relatedVideos.map((video) => {
+            return (
+                <div>
+                    <p>{video.snippet.title}</p>
+                    <img src={video.snippet.thumbnails.medium.url} />
+                </div>
+            )
+        })}
+    </div>
+</div>
+
+)
+
+
+
+export default VideoPage;
